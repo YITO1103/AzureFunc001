c599028b0432a96d9313ee8c03e25672c80ebca5
YUTAJ1103 <yuta.j.1103@gmail.com>
2023-10-15 07:51:55 +0000

    10151651

--- Work01.sql
+++ Work01.sql
@@ -1,4 +1,7 @@
+/*
 
+--curl -s ifconfig.me
+--Server=tcp:server-dev01.database.windows.net,1433;Initial Catalog=db-01;Persist Security Info=False;User ID=yuta.j.1103@gmail.com@server-dev01;Password={your_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;
 -- テーブルの作成
 CREATE TABLE employees (
     id INT PRIMARY KEY NOT NULL,
@@ -13,3 +16,10 @@ VALUES
 (1, 'Taro', 'Yamada', 25),
 (2, 'Hanako', 'Tanaka', 30),
 (3, 'Yuki', 'Suzuki', 28);
+
+
+*/
+
+
+
+
